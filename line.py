aca6ff81a44e37dc51ecaa0c2abe7e15650f55f1
GabrielRF <github@gabrf.com>
2016-07-15 19:15:26 +0000

    Line bot v0

---
+++ line.py
@@ -0,0 +1,31 @@
+import configparser
+import telebot
+
+config = configparser.ConfigParser()
+config.sections()
+config.read('/usr/local/bin/CentralDeTudo/bot.conf')
+
+bot_token = config['BOT']['token']
+msg_dest = config['BOT']['dest']
+bot_admin = config['BOT']['admin']
+line_file = config['LINE']['file']
+
+bot = telebot.TeleBot(bot_token)
+
+def to_line(text):
+   with open(line_file, 'a') as file:
+       file.write(text + '\n')
+       file.close()
+   with open(line_file, 'r') as file:
+       lines = file.readlines()
+       file.close()
+       return len(lines) 
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.from_user.username.lower() in bot_admin.lower():
+        line_size = to_line(message.text)
+        bot.reply_to(message, 'Link adicionado à fila.'
+            + '\nNa fila: ' + str(line_size))
+
+bot.polling(none_stop=True)
