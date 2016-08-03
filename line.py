39466d1098a9225e8a4ca8f94ecc1c12e82eafb3
GabrielRF <github@gabrf.com>
2016-08-03 13:43:35 -0400

    Logging. Check if message is a link

--- line.py
+++ line.py
@@ -1,9 +1,19 @@
 import configparser
+import datetime
+import logging
+import logging.handlers
 import telebot
 
 config = configparser.ConfigParser()
 config.sections()
 config.read('/usr/local/bin/CentralDeTudo/bot.conf')
+
+LOG_INFO_FILE = '/var/log/CentralBot/centralbot.log'
+logger_info = logging.getLogger('InfoLogger')
+logger_info.setLevel(logging.DEBUG)
+handler_info = logging.handlers.RotatingFileHandler(LOG_INFO_FILE,
+    maxBytes=10240, backupCount=5, encoding='utf-8')
+logger_info.addHandler(handler_info)
 
 bot_token = config['BOT']['token']
 msg_dest = config['BOT']['dest']
@@ -25,8 +35,12 @@ def to_line(text):
 def echo_all(message):
     # print(message.text)
     if message.from_user.username.lower() in bot_admin.lower():
-        line_size = to_line(message.text)
-        bot.reply_to(message, 'Link adicionado à fila.'
-            + '\nNa fila: ' + str(line_size))
+        logger_info.info(str(datetime.datetime.now()) + '\t' + str(message.from_user.username) + '\t' + str(message.text))
+        if 'http' in message.text:
+            line_size = to_line(message.text)
+            bot.reply_to(message, 'Link adicionado à fila.'
+                + '\nNa fila: ' + str(line_size))
+        else:
+            bot.reply_to(message, 'Link?')
 
 bot.polling()
